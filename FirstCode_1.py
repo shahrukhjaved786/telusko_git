0ac3c829459141e9c67db4b252427425fa8cfcd5
shahrukhjaved786 <shahrukh.tjit14@gmail.com>
2024-02-03 03:31:22 +0530

    add readme and creds file

--- FirstCode_1.py
+++ FirstCode_1.py
@@ -1,3 +1,12 @@
 print('Hello World')
 print('Hello World, 2nd modification, line two')
 print('Hello World, line three for 3rd modification')
+
+
+print('Username input box added')
+print('added passcode shell')
+
+print('check box for manual input verification is also required')
+
+print("**********************")
+print('Connect to data base via passcode authentication')
